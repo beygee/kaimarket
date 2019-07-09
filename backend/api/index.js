20fae5d0c186f8161ae5fdd0628505e6d3c515a5
김병규 <doug0476@naver.com>
2019-07-09 21:15:16 +0900

    API 서버 구축

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,3 +1,10 @@
 const Koa = require("koa")
 const Router = require("koa-router")
 const router = new Router()
+
+router.get("/", ctx => {
+  console.log("A")
+  ctx.body = "Hello"
+})
+
+module.exports = router
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const Koa = require("koa")
 const Router = require("koa-router")
 const consola = require("consola")
+const koaBody = require("koa-body")
 
 consola.wrapConsole()
 
@@ -9,9 +10,17 @@ const port = 3000
 
 const start = async () => {
   const router = new Router()
+  const api = require("./api")
+
+  app.use(koaBody())
+  router.use("/api", api.routes())
+  app.use(router.routes()).use(router.allowedMethods())
 
   app.listen(port)
-  console.log(`서버 오픈: ${port}`)
+  console.ready({
+    message: `서버 오픈: ${port}`,
+    badge: true
+  })
 }
 
 start()
