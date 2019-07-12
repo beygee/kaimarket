72af972e32d91874377da57df16c278cb787e6c9
김병규 <doug0476@naver.com>
2019-07-12 16:57:16 +0900

    판매 버튼 애니메이션

---
+++ frontend/lib/layout/sell_button.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:week_3/utils/utils.dart';
+import 'package:week_3/styles/theme.dart';
+
+class SellButton extends StatelessWidget {
+  final String text;
+  final IconData icon;
+  final double iconSize;
+  final double fontSize;
+  final VoidCallback onPressed;
+  final double padding;
+  final Color color;
+  final Color bgColor;
+
+  SellButton(
+      {this.text,
+      this.icon,
+      this.onPressed,
+      this.padding,
+      this.iconSize,
+      this.fontSize,
+      this.bgColor = ThemeColor.primary,
+      this.color = Colors.white});
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      padding: EdgeInsets.all(screenAwareSize(padding, context)),
+      shape: CircleBorder(),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Icon(
+            icon,
+            size: screenAwareSize(iconSize, context),
+            color: color,
+          ),
+          Text(text,
+              style: TextStyle(
+                  color: color, fontSize: screenAwareSize(fontSize, context)))
+        ],
+      ),
+      fillColor: bgColor,
+      onPressed: onPressed,
+    );
+  }
+}
---
+++ frontend/lib/layout/sell_overlay.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:week_3/styles/theme.dart';
+import 'package:week_3/utils/utils.dart';
+import 'package:week_3/layout/sell_button.dart';
+import 'dart:math' as math;
+
+class SellOverlay extends StatelessWidget {
+  final Animation listenable;
+  final Animation<Offset> leftAnimation;
+  final Animation<Offset> rightAnimation;
+  final VoidCallback onPressCancel;
+
+  SellOverlay(
+      {this.listenable,
+      this.onPressCancel,
+      this.leftAnimation,
+      this.rightAnimation});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: listenable,
+      builder: (context, child) {
+        return listenable.value > 0
+            ? FadeTransition(
+                opacity: listenable,
+                child: Stack(
+                  fit: StackFit.expand,
+                  alignment: Alignment.bottomCenter,
+                  children: <Widget>[
+                    Positioned.fill(
+                      child: Container(
+                        color: Colors.black26,
+                      ),
+                    ),
+                    Stack(
+                      alignment: Alignment.center,
+                      children: <Widget>[
+                        Positioned(
+                          bottom: screenAwareSize(15.0, context),
+                          child: Transform.translate(
+                            offset: Offset(
+                              -screenAwareSize(
+                                      listenable.value * 70.0, context) *
+                                  math.cos(math.pi / 3.2),
+                              -screenAwareSize(
+                                      listenable.value * 70.0, context) *
+                                  math.sin(math.pi / 3.2),
+                            ),
+                            child: SellButton(
+                              text: "판매",
+                              fontSize: 8,
+                              icon: Icons.add,
+                              iconSize: 20.0,
+                              padding: 8.0,
+                              onPressed: onPressCancel,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: screenAwareSize(15.0, context),
+                          child: Transform.translate(
+                            offset: Offset(
+                              screenAwareSize(
+                                      listenable.value * 70.0, context) *
+                                  math.cos(math.pi / 3.2),
+                              -screenAwareSize(
+                                      listenable.value * 70.0, context) *
+                                  math.sin(math.pi / 3.2),
+                            ),
+                            child: SellButton(
+                              text: "판매",
+                              fontSize: 8,
+                              icon: Icons.add,
+                              iconSize: 20.0,
+                              padding: 8.0,
+                              onPressed: onPressCancel,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: screenAwareSize(15.0, context),
+                          child: SellButton(
+                            text: "취소",
+                            fontSize: 8,
+                            icon: Icons.remove,
+                            iconSize: 20.0,
+                            padding: 15.0,
+                            onPressed: onPressCancel,
+                            bgColor: Colors.white,
+                            color: ThemeColor.primary,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            : Container();
+      },
+    );
+  }
+}
