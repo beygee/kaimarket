67967d4b91ade4dd4575c5d3b58caca76c9b0d36
김병규 <doug0476@naver.com>
2019-07-13 01:36:41 +0900

    도서 판매 페이지 레이아웃

---
+++ frontend/lib/post/post_book_select_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PostBookSelectPage extends StatefulWidget {
+  @override
+  _PostBookSelectPageState createState() => _PostBookSelectPageState();
+}
+
+class _PostBookSelectPageState extends State<PostBookSelectPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
