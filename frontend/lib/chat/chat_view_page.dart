b4e71b9afcfee7d82776ce701cdf6ebee6da3f14
diuni <wldbs0507@kaist.ac.kr>
2019-07-14 21:24:16 +0900

    algorithm update

--- frontend/lib/chat/chat_view_page.dart
+++ frontend/lib/chat/chat_view_page.dart
@@ -5,6 +5,7 @@ import 'dart:developer';
 import 'package:adhara_socket_io/adhara_socket_io.dart';
 import 'package:week_3/utils/utils.dart';
 import 'package:week_3/post/post_view_page.dart';
+import 'package:intl/intl.dart';
 
 class ChatViewPage extends StatelessWidget {
   @override
@@ -50,8 +51,6 @@ class ChatState extends State<Chat> {
     });
     socket.on("message", (data) {
       log.i(data);
-      // db에 메세지 보내기
-      // 
     });
     // Map<String, dynamic> docs;
     // Message message_first =
@@ -69,7 +68,6 @@ class ChatState extends State<Chat> {
 
   @override
   void dispose() {
-    // socket.emit("end", []);
     socketManager.clearInstance(socket);
     log.i("연결 해제");
     super.dispose();
@@ -77,33 +75,47 @@ class ChatState extends State<Chat> {
 
   Future callback() async {
     if (messageController.text.length > 0) {
-      await socket.emit("message", [messageController.text]);
-      // db에 저장도 하기
-      //log.i('add message to db');
+      var now = new DateTime.now();
+      var time = new DateFormat("hh:mm a").format(now);
+      var prev_user = "";
+      var prev_time = "";
+      bool showTime = true;
+      log.i(time);
+      if (prev_user == "user.id" && prev_time == prev_time)
+        showTime = false;
+      await socket.emit("message", [messageController.text, "user_id", time, showTime]);
+      // db에 저장은 소켓이 해준대 ~!
+      // response = await dio.post("message", data: from: user.id, text:messageController.text, time: time, showTime: showTime);
+      prev_user = "user.id";
+      prev_time = time;
       messageController.clear();
       // scrollController.animateTo(scrollController.position.maxScrollExtent,
       //    curve: Curves.easeOut, duration: const Duration(milliseconds: 300));
     }
   }
 
+  Future uploadMessage() async {
+
+  }
+
   Widget _item(context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (context) => PostViewPage()));
-      },
-      child: Container(
+    return Container(
         height: screenAwareSize(80.0, context),
         width: 360.0,
         color: Colors.white,
-        child: Center(
-          child: _itemContent(context),
+        child: Row(
+          children: <Widget>[
+            SizedBox(width: 20.0,),
+            _itemLeft(context),
+            SizedBox(width: 20.0,),
+            _itemMiddle(context),
+            _itemRight(context),
+          ],
         ),
-      ),
     );
   }
 
-  Widget _itemImage(context) {
+  Widget _itemLeft(context) {
     return new Container(
       width: screenAwareSize(60.0, context),
       height: screenAwareSize(60.0, context),
@@ -118,28 +130,37 @@ class ChatState extends State<Chat> {
     );
   }
 
-  Widget _itemContent(context) {
-    return Row(children: <Widget>[
-      SizedBox(width: 20.0,),
-      _itemImage(context),
-      SizedBox(width: 20.0,),
-      Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              if (_postIsSold)
-                Text("[판매완료]", style: _postFont,),
-              Text("통기타", style: _postFont,),
-            ],
-          ),
-          Text("15,500원", style: _postFont,),
-        ],
-      ),
-    ],);
+  Widget _itemMiddle(context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            if (_postIsSold)
+              Text("[판매완료]", style: _postFont,),
+            Text("통기타", style: _postFont,),
+          ],
+        ),
+        Text("15,500원", style: _postFont,),
+      ],
+    );
   }
 
+  Widget _itemRight(context){
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => PostViewPage()));
+      },
+      child: Container(
+        height: screenAwareSize(80.0, context),
+        width: 80.0,
+        color: Colors.black,
+        child: Text("게시글\n확인하기")
+      ),
+    );
+  }
   Widget _typingbar(context) {
     return Container(
       width: 270.0,
@@ -186,6 +207,7 @@ class ChatState extends State<Chat> {
                   controller: scrollController,
                   children: <Widget>[
                     // db에서 가져오기
+                    // Response<Message> response = await dio.get<Message>("message", ??).toList();
                     new Message(
                       from: 'diuni',
                       text: 'heello 내 이름은 지윤팍팍 아임 지윤 유 쎄이 지 아 쎄 윤 지 윤 지 윤',
