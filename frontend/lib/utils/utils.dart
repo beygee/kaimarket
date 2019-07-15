7e775787574a178f2cd30f7769fcc7e54504bd5a
김병규 <doug0476@naver.com>
2019-07-16 05:40:25 +0900

    아바타 부여

--- frontend/lib/utils/utils.dart
+++ frontend/lib/utils/utils.dart
@@ -24,3 +24,12 @@ String getMoneyFormat(int price) {
   final numberFormat = new NumberFormat("#,##0", "en_US");
   return numberFormat.format(price);
 }
+
+//유저 아이디를 통해 아스키 코드 알고리즘을 통해 아바타 이미지 만들기
+String getRandomAvatarUrlByPostId(String postId) {
+  //뒤 4글자를 아스키코드값으로 변환해 모두 더한뒤 나머지 9
+  log.i(postId.hashCode);
+  int idx = postId.hashCode % 9;
+
+  return 'assets/avatar/$idx.png';
+}
