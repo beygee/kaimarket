4b1773fd89215fe98cdc089b80458fa97a06bdbf
diuni <wldbs0507@kaist.ac.kr>
2019-09-22 22:30:54 +0900

    신고하기 넣는 중

--- frontend/ios/Flutter/flutter_export_environment.sh
+++ frontend/ios/Flutter/flutter_export_environment.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-<<<<<<< HEAD
 export "FLUTTER_ROOT=/Users/jiyun/Documents/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/jiyun/Documents/kaimarket/frontend"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/jiyun/Documents/kaimarket/frontend/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/jiyun/Documents/flutter/bin/cache/artifacts/engine/ios"
-=======
-export "FLUTTER_ROOT=/Users/bangu/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/bangu/Documents/VSCode/madcamp/third/frontend"
-export "FLUTTER_TARGET=lib/main.dart"
-export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/bangu/Documents/flutter/bin/cache/artifacts/engine/ios"
->>>>>>> 347c6e4228c2e01aa9fa43b64a10936b8cd6ba5b
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
